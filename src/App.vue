29acdf8d1af1bb2edabc5f81479feffe09b97600
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-02-14 19:10:18 +0600

    practice from handling

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <SignUpForm />
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+export default {
+  name:'App',
+  components:{
+    SignUpForm,
+  }
+}
+</script>
+
+<style>
+
+
+body{
+  margin: 0;
+  background: #eee;
+}
+
+
+</style>
